4c90c306cfadc9e1a8ed71b1c242e7b1447a9609
Monika Grzanek <grzanekmonika@o2.pl>
2024-04-24 13:21:08 +0200

    Init project and add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+function Footer(){
+    return (
+        <div>
+            <footer>
+                <ul>
+                    <li>Regulamin</li>
+                    <li>FAQ</li>
+                    <li>O nas</li>
+                </ul>
+            </footer>
+        </div>
+    )
+}
