d45d98ee8ed4bb5bff4eca1d9301d184633a5802
Monika Grzanek <grzanekmonika@o2.pl>
2024-04-25 14:37:26 +0200

    Add loops and conditions in Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,17 +2,46 @@ import React from 'react';
 import './footer.css';
 import Copyright from '../Copyright/Copyright.js';
 
+
+const elements1 = ["TOS", "About"];
+const items = [];
+const elements2 = ["contact", "legal"];
+
+
 function Footer(props){
+    for( const [index, value] of elements1.entries()) {
+        items.push(<li>{value}</li>);
+    }
+
+    let isLogged = false;
+    let button;
+    if(isLogged) {
+        button = <button>Logout</button>;
+    } else {
+        button = <button>Login</button>;
+    }
+
     return (
         <div>
             <footer>
                 <ul>
-                    <li>Regulamin</li>
-                    <li>FAQ</li>
+                    {items}
                     <li>Contact: {props.contactEmail}, 
-                        {props.companyData.city} </li>
+                        {props.companyData.city}
+                    </li>
+                    {elements2.map( (value, index) => {
+                        return <li>{value}</li>
+                    } )}
                 </ul>
             <Copyright year='202X' />
+            {button}
+            {isLogged === true &&
+            <div>Sprawdź pocztę!</div>}
+
+            {isLogged 
+                ? <div>Zobacz notyfikacje</div>
+                : <div>Zaloguj się dla notyfikacji</div>
+            }
             </footer>
         </div>
     )
